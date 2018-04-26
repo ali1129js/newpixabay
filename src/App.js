876cb0607a31978af48c5b510c92803ea91bd45c
ali1129js <ali1129js@gmail.com>
2018-04-26 17:50:05 +0200

    conitional rendering simple if

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@
  * @Author: Ali
  * @Date:   2018-04-25T22:06:52+02:00
  * @Last modified by:   Ali
- * @Last modified time: 2018-04-25T23:01:48+02:00
+ * @Last modified time: 2018-04-26T17:48:50+02:00
  */
 
 import React, { Component } from 'react'
-
+import Grid from './Components/Grid'
 const API_KEY = '8804245-155ed4ab1b84647eeb9fbf6ad'
-const endpoint ="https://pixabay.com/api/?key="+API_KEY
+const endpoint ="https://pixabay.com/api/?key="+API_KEY+"&q="+encodeURIComponent('roses')
 const fetchOption = {
   method:'GET'
 }
@@ -19,27 +19,37 @@ class App extends Component {
 
     }
   }
-componentWillMount() {
+componentDidMount() {
     fetch(endpoint, fetchOption)
     .then(d => d.json())
     .then(d => {
-      this.setState({pixabayData: d
-      })
+      this.setState({pixaBay:d})
     })
     .catch(err => {
       console.error(err);
     })
 }
-  render(){
-    if(this.state.pixabayData){
-      const data = this.state
+
+  render() {
+    if(!this.state.pixaBay){
+      return <div className="loading">loading</div>
     }
-    return (
-      {data.map(block =>
-        <li> {block.user} </li>
-      )}
+    console.log(this.state.pixaBay.hits)
+    return(
+      <Grid data={this.state.pixaBay}/>
     )
   }
 }
 
 export default App
+/*
+<div className="ll">{data.map(block =>
+    <li key={block.id}> {block.user} </li>
+)}</div>
+
+return (
+  <div>
+    <h1>{block.user}</h1>
+  </div>
+)
+*/
---
+++ src/Components/Grid.js
@@ -0,0 +1,16 @@
+/**
+ * @Author: Ali
+ * @Date:   2018-04-26T15:09:30+02:00
+ * @Last modified by:   Ali
+ * @Last modified time: 2018-04-26T17:49:16+02:00
+ */
+import React,{ Component } from 'react'
+class Grid extends Component {
+
+  render(){
+    return(
+      <h1>  Grid here</h1>
+    )
+  }
+}
+export default Grid
