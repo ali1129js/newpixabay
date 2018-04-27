1004cd70c89bdad2fdc575ff449301950039f5f6
ali1129js <ali1129js@gmail.com>
2018-04-27 21:13:22 +0200

    new ui

--- src/App.js
+++ src/App.js
@@ -2,16 +2,14 @@
  * @Author: Ali
  * @Date:   2018-04-25T22:06:52+02:00
  * @Last modified by:   Ali
- * @Last modified time: 2018-04-27T14:34:24+02:00
+ * @Last modified time: 2018-04-27T19:37:56+02:00
  */
-
 import React, { Component, Fragment } from 'react'
 import Input from './Components/Input'
 import Grid from './Components/Grid'
 
 
 const pixabayapi = "https://pixabay.com/api/?key=8804245-155ed4ab1b84647eeb9fbf6ad"
-
 const searchTerm = encodeURIComponent('young')
 const endpoint =pixabayapi+"&q="+searchTerm
 const fetchOption = {
@@ -21,7 +19,7 @@ class App extends Component {
   constructor (){
     super()
     this.state = {
-      endpoint:endpoint
+      
     }
     this.handleInput = this.handleInput.bind(this)
   }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+/**
+ * @Author: Ali
+ * @Date:   2018-04-27T19:36:44+02:00
+ * @Last modified by:   Ali
+ * @Last modified time: 2018-04-27T19:36:44+02:00
+ */
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import registerServiceWorker from './registerServiceWorker'
+
+ReactDOM.render(<App />, document.getElementById('root'))
+registerServiceWorker()
