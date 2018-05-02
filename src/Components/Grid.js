6114ccbdf16b12e9a60e3b321350aeb06c278e8a
ali1129js <ali1129js@gmail.com>
2018-05-02 12:17:00 +0200

    margin fixed

--- src/Components/Grid.js
+++ src/Components/Grid.js
@@ -2,12 +2,12 @@
  * @Author: Ali
  * @Date:   2018-04-26T15:09:30+02:00
  * @Last modified by:   Ali
- * @Last modified time: 2018-05-02T10:36:17+02:00
+ * @Last modified time: 2018-05-02T12:12:42+02:00
  */
 import React,{ Component } from 'react'
 import { Card } from 'material-ui'
 import Avatar from 'material-ui/Avatar';
-
+import Paper from 'material-ui/Paper'
 class Grid extends Component {
   render(){
     if(!this.props.data){
@@ -17,29 +17,31 @@ class Grid extends Component {
       }
       const list = this.props.data.map(block =>
         <ul key={block.id}>
-          <Card>
+          <Card style={{height:305,width:400}} zDepth={5} >
             <Avatar src={block.userImageURL} />
             <img
               src={block.largeImageURL}
               alt={block.user}
-              height="270"
-              width="480"
+              height="210"
+              width="380"
             />
             <div>
             </div>
             <div className="card-data">
               <ul>
-                <li>{block.likes} Likes</li>
-                <li>{block.views} views</li>
-                <li>{block.downloads} downloads</li>
-                <li>{block.comments} comments</li>
+                <li >{block.likes}favorite</li>
+                <li >{block.views}visibility</li>
+                <li >{block.downloads}</li>
+                <li >{block.comments}</li>
               </ul>
             </div>
-          </Card><br />
+          </Card>
         </ul>
       )
     return(
-      <ul> {list} </ul>
+      <ul>
+        <Paper zDepth={2}> {list} </Paper>
+        </ul>
     )
   }
 }
