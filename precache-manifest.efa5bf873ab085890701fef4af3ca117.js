9a0795ee48b7cfb45a06d8b2bcf9908e6327da9b
ali1129js <ali1129js@gmail.com>
2020-02-17 18:15:22 +0100

    Updates

---
+++ precache-manifest.efa5bf873ab085890701fef4af3ca117.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "454591a24fee32213a2e",
+    "url": "/newpixabay/static/js/runtime~main.454591a2.js"
+  },
+  {
+    "revision": "c99a2407716deb2628cd",
+    "url": "/newpixabay/static/js/main.c99a2407.chunk.js"
+  },
+  {
+    "revision": "4e2972bab64b83944fe9",
+    "url": "/newpixabay/static/js/1.4e2972ba.chunk.js"
+  },
+  {
+    "revision": "c99a2407716deb2628cd",
+    "url": "/newpixabay/static/css/main.9c2b12ce.chunk.css"
+  },
+  {
+    "revision": "4e2972bab64b83944fe9",
+    "url": "/newpixabay/static/css/1.68555509.chunk.css"
+  },
+  {
+    "revision": "5c250157d055d300ae03e0393491f61a",
+    "url": "/newpixabay/index.html"
+  }
+];
